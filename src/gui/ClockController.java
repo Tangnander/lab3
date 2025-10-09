e4e585d2aa0a04427135672364dcf017837dbb5e
johanssonviktor1 <viktor@kyrkasen.se>
2025-10-09 12:56:35 +0200

    Update ClockController.java

--- src/gui/ClockController.java
+++ src/gui/ClockController.java
@@ -1,35 +1,59 @@
 package gui;
 
 import clock.WeekAlarmClock;
+import time.TimeType;
 
-public class ClockController {
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
-    private boolean clockRunning = true;
-    WeekAlarmClock alarmClock = new WeekAlarmClock();
+public  class ClockController {
 
-    public ClockController() {
+    private boolean clockRunning = false;
+    private WeekAlarmClock alarmClock = new WeekAlarmClock();
+    private Thread clockThread;
+    private JLabel timeLabel; // JLabel från GUI
 
+    public ClockController(JLabel timeLabel) {
+        this.timeLabel = timeLabel;
+        updateLabel(); // Visa initial tid
     }
 
     public void startClock() {
-        //This starts the clock and updates the label in MainTab
-        while (clockRunning = false) {
-            alarmClock.tickTack();
-            MainTab.currentTime.setText(alarmClock.toString());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    public void setClockRunning() {
+        if (clockRunning) return; 
         clockRunning = true;
+
+        clockThread = new Thread(() -> {
+            while (clockRunning) {
+                alarmClock.tickTack();
+                updateLabel();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        });
+        clockThread.start();
     }
 
-    public boolean getClockRunning() {
-        return clockRunning;
+    public void stopClock() {
+        clockRunning = false;
     }
 
+    public void resetClock() {
+        stopClock();
+        alarmClock = new WeekAlarmClock(); // skapa en ny klocka
+        updateLabel();
+    }
+    
+    public void setTime(TimeType time) {
+        alarmClock.setTime(time);
+        updateLabel();
+    }
+
+
+
+    private void updateLabel() {
+        SwingUtilities.invokeLater(() -> timeLabel.setText(alarmClock.toString()));
+    }
 }
